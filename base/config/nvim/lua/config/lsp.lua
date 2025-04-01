ad4a3d198ba90ad5b2bb08437b30db057dad35b0
Justin Kirkegaard <justin.kirkegaard@growersedge.com>
2025-03-31 21:32:21 -0500

    refactor: Remove legacy config files
    Remove old configuration files that have been relocated to follow XDG Base Directory specification:
    - Old tmux config files
    - Old zsh config files
    - Old Neovim configuration structures
    - Old p10k configuration file

--- base/config/nvim/lua/config/lsp.lua
+++
@@ -1,4 +0,0 @@
--- Load the base LSP configuration module
-require("config.lsp-base")
-
--- This file can be used to add global LSP settings that apply to all images
--- base/config/nvim/lua/config/treesitter.lua
+++
@@ -1,4 +0,0 @@
--- Load the base Treesitter configuration module
-require("config.treesitter-base")
-
--- This file can be used to add global Treesitter settings that apply to all images
